31831d4aefa08f7b3abc12ac64bfb27372ba3937
francescacallard <francescalily@outlook.com>
2024-01-25 12:38:38 +0000

    push

--- src/pages/Chat/index.jsx
+++ src/pages/Chat/index.jsx
@@ -1,18 +1,51 @@
-import React, {useState} from "react";
-
+import React, { useState } from "react";
 import {Message, Input, History, Clear} from "../../components"
-import "./styles.css"
+import "./styles.css";
 
-export default function App() {
-  const [input, setInput] = useState('')
+export default function Chat() {
+  const [input, setInput] = useState("");
   const [messages, setMessages] = useState([]);
   const [history, setHistory] = useState([]);
+  const clear = () => {
+    setMessages([]);
+    setInput("");
+  };
 
   return (
     <div className="App">
-      <div className="Coloumn">
+      <div className="Column">
         <h3 className="Title">Chat Messages</h3>
+        <div className="Content">
+          {messages.map((el, i) => {
+            return <Message key={i} role={el.role} content={el.content} />;
+          })}
+        </div>
+        <Input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          onClick={input ? handleSubmit : undefined}
+        />
+      </div>
+      <div className="Column">
+        <h3 className="Title">History</h3>
+        <div className="Content">
+          {history.map((el, i) => {
+            return (
+              <History
+                key={i}
+                question={el.question}
+                onClick={() =>
+                  setMessages([
+                    { role: "user", content: history[i].question },
+                    { role: "assistant", content: history[i].answer },
+                  ])
+                }
+              />
+            );
+          })}
+        </div>
+        <Clear onClick={clear} />
       </div>
     </div>
-  )
+  );
 }
