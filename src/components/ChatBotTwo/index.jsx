091f6ab04cd2c7bb03e00dc43e96a54f8b6131b3
francescacallard <francescalily@outlook.com>
2024-02-14 13:04:44 +0000

    able to get rid of line

--- src/components/ChatBotTwo/index.jsx
+++ src/components/ChatBotTwo/index.jsx
@@ -127,7 +127,7 @@ const handleButtonClicked = async (question) => {
 </MessageList>
 
 
-                <MessageInput attachButton={false} placeholder='Type your response...' onSend={handleSend}/>
+                <MessageInput style={{borderTop: "none"}}attachButton={false} placeholder='Type your response...' onSend={handleSend}/>
             </ChatContainer>
             
         </MainContainer>
